12860f1bb71252570bcfd04b0fbc0818d9a79f4b
Daniel Trương <khicononline9xtq@icloud.com>
2021-06-17 09:17:32 +0700

    ÚpTE 17/06

---
+++ BASE/product-slider/welcome/settings.js
@@ -0,0 +1,25 @@
+[   
+     {
+         tab_name:"General",
+         settings: [
+             {
+                 type : "text",
+                 name:"title",
+                 label: "Block title",
+                 value: "Welcom to LayoutHub development team"
+             },
+             {
+                 type: 'image',
+                 name: 'logo',
+                 label: 'Logo',
+                 value: '%URL%assets/logo.png'
+             },
+             {
+                 type: "textarea",
+                 label: "Description text",
+                 name: "description",
+                 value: "LayoutHub is the best page builder for Shopify"
+             }
+         ]
+     }
+]
