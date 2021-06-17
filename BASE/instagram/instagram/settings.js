331e68ae170ad8ccdcf63402c9502820efee8e7a
Daniel Trương <khicononline9xtq@icloud.com>
2021-06-17 11:48:08 +0700

    Instagram

---
+++ BASE/instagram/instagram/settings.js
@@ -0,0 +1,25 @@
+[{
+    name: 'title',
+    type: 'text',
+    label: 'Title',
+    value: 'Follow us on instagram',
+
+},
+
+{
+    type: "instagram",
+    name: "instagram",
+    label: "Instagram access token",
+    value: ""
+},
+{
+    type: "number_slider",
+    name: "limit",
+    label: "Number of photo",
+    value: 12,
+    options: {
+        min: 1,
+        max: 50
+    }
+}
+]
